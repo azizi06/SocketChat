faea378d7880b5c2934b6b396d3400e73a6390d5
AZIZImohammed <artazizi06@gmail.com>
2026-06-24 22:55:49 +0200

    init commit

---
+++ Client.java
@@ -0,0 +1,14 @@
+class Client{
+    public static void main(String[] args) {
+        String host="127.0.0.1";
+        int port=5000; 
+
+        try (
+            
+        ) {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
